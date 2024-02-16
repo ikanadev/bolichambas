06b36a53d1ce80ec539cefdb6d2e2eac80bf4711
Kevin Vargas <vargaskevv@gmail.com>
2024-02-16 19:43:40 -0400

    Add daysi ui

--- boliempleos/src/App.tsx
+++ boliempleos/src/App.tsx
@@ -1,9 +1,16 @@
 function App() {
-  return (
-    <>
-      <h1 class="text-3xl font-bold">Hello world!</h1>
-    </>
-  )
+	return (
+		<>
+			<h1 class="text-3xl font-bold">Hello world!</h1>
+			<div class="flex gap-2">
+				<button class="btn">Press me!</button>
+				<button class="btn btn-primary">Press me!</button>
+				<button class="btn btn-secondary">Press me!</button>
+				<button class="btn btn-outline">Press me!</button>
+				<button class="btn">Press me!</button>
+			</div>
+		</>
+	)
 }
 
 export default App
--- boliempleos/tailwind.config.js
+++ boliempleos/tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+	content: [
 		"./index.html",
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				dark: {
+					...require("daisyui/src/theming/themes")["sunset"],
+					primary: "#2F4A6F",
+					secondary: "#929a8F",
+					accent: "#3D5A80",
+					"--rounded-box": "0.5rem",
+					"--rounded-btn": "0.25rem",
+				},
+			},
+		],
+	}
 }
 
