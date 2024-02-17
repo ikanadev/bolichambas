c372a90f589fdc1807a730ecb04c25f1316a6f3d
Kevin Vargas <vargaskevv@gmail.com>
2024-02-17 00:32:59 -0400

    Add app icons

---
+++ boliempleos/src/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Location } from "./Location";
+export { default as Buildings } from "./Buildings";
+export { default as Calendar } from "./Calendar";
+export { default as OpenInNew } from "./OpenInNew";
+export { default as DegreeHat } from "./DegreeHat";
